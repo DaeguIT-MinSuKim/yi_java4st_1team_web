6a1f108928923ba661388e5487e57a5358bb9960
kimdeahun6317 <kdh0161@naver.com>
2020-10-12 14:33:01 +0900

    designer home

--- Scripts/07 eventTest.sql
+++ Scripts/07 eventTest.sql
@@ -2,4 +2,5 @@ SELECT * FROM EVENT;
 DELETE FROM EVENT e ;
 
 --이벤트test정보 insert
-INSERT INTO EVENT(EVENT_NAME,EVENT_START,EVENT_END,EVENT_PIC,EVENT_CONTENT) VALUES ('cvg',sysdate,sysdate,'event/images/movieCoupon.jpg','헤어랑샵과 cgv의 콜라보'); 
+INSERT INTO EVENT(EVENT_NAME,EVENT_START,EVENT_END,EVENT_PIC,EVENT_CONTENT) VALUES ('CGV',sysdate,sysdate,'event/images/movieCoupon.jpg','헤어랑샵과 cgv의 콜라보'); 
+INSERT INTO EVENT(EVENT_NAME,EVENT_START,EVENT_END,EVENT_PIC,EVENT_CONTENT) VALUES ('SummerEvent',sysdate,sysdate,'event/images/summer.jpg','여름한정 이벤트 대축제~'); 
