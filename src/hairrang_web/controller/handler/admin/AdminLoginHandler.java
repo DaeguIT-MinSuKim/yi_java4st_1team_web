d88b69f622b32e08a2fc5455489ed7a28e753a1b
jisu25 <kimpigu.zz@gmail.com>
2020-10-21 14:18:00 +0900

    admin css

---
+++ src/hairrang_web/controller/handler/admin/AdminLoginHandler.java
@@ -0,0 +1,28 @@
+package hairrang_web.controller.handler.admin;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import hairrang_web.controller.Command;
+
+public class AdminLoginHandler implements Command{
+
+	@Override
+	public String process(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		
+		String url = "/admin/admin_login.jsp";
+		
+		if(request.getMethod().equalsIgnoreCase("GET")) {
+			System.out.println(getClass().getSimpleName() + ">> GET");
+			
+		} else {
+			System.out.println(getClass().getSimpleName() + ">> POST");
+			
+		}
+		
+		return url;
+	}
+}
